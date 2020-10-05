26135e3eb614b0dfd9d4ea351e9006dfcacea736
bom2013 <noam21288@gmail.com>
2020-10-05 16:42:07 +0000

    Fix empty ast problem

--- Interpreter.py
+++ Interpreter.py
@@ -5,10 +5,13 @@ This file needed only for the codewars kata
 from Scanner import Scanner
 from Parser import Parser
 
+
 class Interpreter:
     def input(self, code):
         scanner = Scanner(code)
         tokens = scanner.scan()
         parser = Parser(tokens)
         ast = parser.parse()
+        if not ast:
+            return ""
         return ast.run()
